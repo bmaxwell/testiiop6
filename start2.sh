b0124ee8742d21e8f139cb6ba9c947635b635adc
Brad Maxwell <bmaxwell@redhat.com>
2019-01-08 16:15:54 -0600

    updates start* scripts

--- start2.sh
+++ start2.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-#./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 -Djboss.socket.binding.port-offset=100 -Djava.security.policy=test/java.policy -Djava.security.auth.login.config=test/appclientlogin.conf -Dcom.sun.CORBA.ORBUseDynamicStub=true -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
-
-# does not work
-#./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone2 -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100 
-
-# works
-./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone2 -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100 -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
+# make sure  <initializers security="client" transactions="spec"/> has security=client
+./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone2 -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100 
