8a0580198de77118264f663fe927aa616e272f93
Brad Maxwell <bmaxwell@redhat.com>
2019-01-08 16:03:23 -0600

    updated client and start scripts

--- start1.sh
+++ start1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 #./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 -Djava.security.policy=test/java.policy -Djava.security.auth.login.config=test/appclientlogin.conf -Dcom.sun.CORBA.ORBUseDynamicStub=true -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
+
+# does not work
+#./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 
+
+# works
 ./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
--- start2.sh
+++ start2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 #./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 -Djboss.socket.binding.port-offset=100 -Djava.security.policy=test/java.policy -Djava.security.auth.login.config=test/appclientlogin.conf -Dcom.sun.CORBA.ORBUseDynamicStub=true -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
-./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone -Djboss.node.name=node1 -Djboss.socket.binding.port-offset=100 -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
+
+# does not work
+#./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone2 -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100 
+
+# works
+./bin/standalone.sh -c standalone-full.xml -Djboss.server.base.dir=standalone2 -Djboss.node.name=node2 -Djboss.socket.binding.port-offset=100 -Dorg.omg.PortableInterceptor.ORBInitializerClass.org.wildfly.iiop.openjdk.csiv2.SASClientInitializer
